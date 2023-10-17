8c80fdff9ae7113d5e8f610caeb565607a5fb6d6
David Garcia Varas <davidga2@c2r7s4.42madrid.com>
2023-10-18 00:11:16 +0200

    FIN

--- bonus/heredoc_bonus.c
+++ bonus/heredoc_bonus.c
@@ -6,7 +6,7 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 20:52:58 by davidga2          #+#    #+#             */
-/*   Updated: 2023/10/17 20:50:31 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/10/17 23:59:29 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_heredoc_write_loop(char *limiter, int heredoc_fd)
 {
-	char *writed_line;
+	char	*writed_line;
 
 	while (1)
 	{
@@ -30,13 +30,8 @@ void	ft_heredoc_write_loop(char *limiter, int heredoc_fd)
 		write(heredoc_fd, writed_line, ft_strlen(writed_line));
 		free(writed_line);
 	}
-	free(limiter);
 }
 
-// Documenta esto, men.
-// Añade protecciones al crear o borrar el heredoc, gracias vicmarti.
-//
-// apped
 int	ft_heredoc(char **argv)
 {
 	int		heredoc_fd;
@@ -49,26 +44,17 @@ int	ft_heredoc(char **argv)
 	if (heredoc_fd == -1)
 		ft_error("Failure opening the temporal file used in heredoc");
 	ft_heredoc_write_loop(limiter, heredoc_fd);
+	free(limiter);
 	close(heredoc_fd);
 	heredoc_fd = open(".heredoc", O_RDONLY);
 	return (heredoc_fd);
 }
 
-// Función dedicada al primer subproceso/hijo, encargado de crear el archivo
-// temporal en el que se replicará el funcionamiento del heredoc a modo de
-// infile.
-// Se crea el subproceso y dentro del mismo se asigna el fd del archivo
-// temporal del heredoc a la variable heredoc_fd mediante la función
-// ft_heredoc.
-// Se cierra el fd del extremo del pipe con el que no interactua.
-// Se redireccionan los fd in y out con el fd del heredoc y el extremo de
-// escritura del pipe respectivamente y después se cierran. Finalmente se
-// ejecuta el comando introducido.
 void	ft_heredoc_child(char **argv, char **envp, int *pipe)
 {
 	pid_t	pid;
 	int		heredoc_fd;
-	
+
 	pid = ft_fork_manage();
 	if (!pid)
 	{
@@ -83,18 +69,6 @@ void	ft_heredoc_child(char **argv, char **envp, int *pipe)
 	ft_wait_childs(1);
 }
 
-// Función dedicada al subproceso/hijo final.
-// Crea y gestiona el subproceso.
-// Dentro del subproceso:
-// Borra el archivo oculto dedicado al heredoc para evitar tener archivos
-// basura tras la ejecución del programa.
-// Crea (con open) un archivo en el que volcar el resultado de la ejecución
-// del programa asociado al comando introducido, o lo abre en caso de existir.
-// Cierra el extremo del pipe con el que no interactua.
-// Redirecciona los fd de entrada y salida al fd extremo del lectura del pipe
-// y el fd del outfile respectivamente.
-// Cierra los fd por los que se "dupeado".
-// Ejecuta el comando introducido mediante ft_exec.
 void	ft_outfile_child_heredoc(char **argv, char **envp, int *pipe)
 {
 	pid_t	pid;
@@ -117,12 +91,6 @@ void	ft_outfile_child_heredoc(char **argv, char **envp, int *pipe)
 	}
 }
 
-// Como en el subject no se pone un ejemplo usando multi-piping, en el bonus
-// del heredoc solo uso un pipe.
-// Desde el proceso padre se crea el pipe y se llaman a las funciones que
-// ejecutarán a los hijos(subprocesos). Después se cierra el pipe,
-// no por gusto ni una buena práctica, sino porque si no lo cerramos no va.
-// Y espera a que termine la ejecución del último hijo.
 void	ft_pipex_heredoc(char **argv, char **envp)
 {
 	int	pipe[2];
--- bonus/pipex_bonus.c
+++ bonus/pipex_bonus.c
@@ -6,18 +6,12 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 23:37:02 by davidga2          #+#    #+#             */
-/*   Updated: 2023/10/15 23:21:30 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/10/17 23:51:24 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex_bonus.h"
 
-// Función encargada de esperar a tantos subprocesos hijos como el número
-// entero que se reciba por el parámetro de la función. "waitpid(-1, NULL, 0)"
-// espera a cualquier subproceso/hijo dentro de la totalidad de
-// subprocesos/hijos en ejecución. Solo espera a uno dentro de todo el grupo,
-// por lo que si queremos esperar a varios hijos deberá ejecutarse tantas veces
-// como hijos a esperar, de hay que esté anidada en un bucle.
 void	ft_wait_childs(int total_childs)
 {
 	while (total_childs > 0)
@@ -27,20 +21,12 @@ void	ft_wait_childs(int total_childs)
 	}
 }
 
-// Función para la gestión de errores en la creación de un pipe.
-// Permite ahorrar una línea a la hora de crear un pipe.
-// Recibe por parámetro los 2 fd que se usarán para su vinculación
-// y creación del pipe.
 void	ft_pipe_manage(int *fd)
 {
 	if (pipe(fd) == -1)
 		ft_error("fds linking at pipe creation has failed");
 }
 
-// Función encargada de cerrar ambos extremos del pipe recibido
-// por parámetro.
-// Usada principalmente en el proceso padre y creada para
-// ahorrar líneas de código de cara a la Norma.
 void	ft_pipe_close(int *fd)
 {
 	if (close(fd[0]) == -1)
@@ -49,27 +35,6 @@ void	ft_pipe_close(int *fd)
 		ft_error("Failure closing pipe_fd[1]");
 }
 
-// Como en el bonus se incluye el multi-pipe, hay un nuevo subproceso hijo
-// encargado de comunicarse con dos pipes; recibiendo información de un pipe
-// y volcando el resultado de su ejecución de subproceso en otro pipe con sus
-// debidas redirecciones.
-// Siempre se usarán tantos pipes menos 1 como comandos se hayan introducido,
-// aunque realmente solo coexistirán dos pipes al mismo tiempo durante la
-// ejecución del programa en caso de introducir más de 2 comandos debido al
-// funcionamiento del hijo intermedio.
-// Por ello el proceso padre necesita declarar dos variables dedicadas a la
-// creación de los dos posibles pipes que coexistirán al mismo tiempo.
-// En caso de haber multi-piping se hará uso del hijo intermedio tantas veces
-// como hijos intermedios haya. Esto se calcula restándole 5 al número total
-// de argumentos.
-// El número total de hijos serán los argumentos menos 3.
-// En caso de necesitar multi-piping se creará el segundo pipe, se ejecutará
-// la función del hijo intermedio, se cerrará el primer pipe y el segundo
-// pipe pasará a ser el primero para que el siguiente hijo intermedio o el
-// hijo final lo traten debidamente.
-// middle_childs es el contador de hijos intermedios que han de ejecutarse.
-// cmd_count es la posición del argumento a usar como comando dentro de los
-// hijos intermedios e hijo final.
 void	ft_pipex(char **argv, char **envp, int total_childs, int middle_childs)
 {
 	int		left[2];
@@ -94,23 +59,6 @@ void	ft_pipex(char **argv, char **envp, int total_childs, int middle_childs)
 	ft_wait_childs(total_childs);
 }
 
-// El main se encarga de llamar a una función u otra según se use
-// single-pipe o multi-pipes con ft_pipex o en su defecto el heredoc,
-// que solo usa singe-pipe según el ejemplo del subject con ft_pipe_heredoc.
-//
-// El condicional encargado de ejecutar la función dedicada al heredoc
-// verifica que el segundo argumento introducido sea solo "here_doc" y nada
-// más, por ello se pone "9", ara que comprare los carácteres del string más
-// el caracter nulo que lo cierra, es decir, "here_doc\0". En caso de recibir
-// "here_doca", por ejemplo, se ejecutaría el pipex base con el bonus del
-// multi-pipe.
-//
-// Estructura en la ejecución con un solo pipe:
-// 				"./pipex_bonus infile "cmd1" "cmd2" outfile"
-// Estructura en la ejecución del multi-pipe:
-//				"./pipex_bonus infile "cmd1" "cmd2" "cmd3" "cmd(...)" outfile"
-// Estructura de la ejecución del heredoc:
-// 				"./pipex_bonus here_doc LIMITER "cmd1" "cmd2" outfile"
 int	main(int argc, char **argv, char **envp)
 {
 	if (argc >= 5)
